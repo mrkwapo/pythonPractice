807653bd3d28819e6931a1d5f194ea867704df27
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2019-12-20 09:45:43 -0500

    done

--- caesarx.py
+++ caesarx.py
@@ -1,38 +1,39 @@
-# how to access the command line argument
+# This program ciphers a plain text message by shifting each character by the given key (which is a number)
 import sys
 import cs50
 
-#validating length of command line arguments
-if not len(sys.argv)== 2:
-  print("Usage: python caesar.py key")
-  exit(1)
+# validating length of command line arguments
+if not len(sys.argv) == 2:
+    print("Usage: python caesar.py key")
+    exit(1)
 
-#getting plaintext from the user
+# getting plaintext from the user
 plaintext = cs50.get_string("plaintext: ")
 print("ciphertext: ", end='')
 
-# initializing the argument into a variable and casting it as a number
+# initializing the key argument, casting it as a number and handling the ASCII wrap around with the Modulo operator
 key = int(sys.argv[1]) % 26
 
-for i in range(0, len(plaintext),1):
-  encrypt = key + plaintext[i]
+# iterating through each charcter in the plain text message
+for i in range(0, len(plaintext), 1):
+    encrypt = key + ord(plaintext[i])
 
-    #if there is a space print space
-if plaintext[i].isalpha() == False:
+    # handling non alphabetic characters
+    if plaintext[i].isalpha() == False:
         print(plaintext[i], end='')
 
     # handling lowercase
-if plaintext[i].islower():
+    if plaintext[i].islower():
         if encrypt < 122:
-            print(key + plaintext[i])
+            print(chr(key + ord(plaintext[i])), end='')
         elif encrypt > 122:
-            print( plaintext[i] - (26 - key))
+            print(chr(ord(plaintext[i]) - (26 - key)), end='')
 
-    #handling uppercase
-if plaintext[i].isupper():
+    # handling uppercase
+    if plaintext[i].isupper():
         if encrypt < 90:
-            print(key + plaintext[i])
+            print(chr(key + ord(plaintext[i])), end='')
         elif encrypt > 90:
-            print(plaintext[i] - (26 - key))
+            print(chr(ord(plaintext[i]) - (26 - key)), end='')
 
 print()
